1613e42d18cb6ba877c27715f18bff7c9c41895e
BRULAIS Sébastien <sebastien.brulais@gmail.com>
2023-10-16 15:16:49 +0200

    Macro pilot last comit

--- Marco_Pilot/marco_pilot.ino
+++ Marco_Pilot/marco_pilot.ino
@@ -1,3 +1,12 @@
+/* 
+ *  Projet Marco Pilot: asservissement du cap et de l'altitude
+ *  
+ *  Pour programmer la carte choisir:
+ *    - la cible Ardumega 2560
+ *    - la vitesse du port serie 250000 bauds
+*/
+
+/*Importation des librairies*/
 #include "MS5611.h"
 #include <SPI.h>
 #include "MeanFilterLib.h"
@@ -7,62 +16,86 @@
 #include <Adafruit_HMC5883_U.h>
 #include <PID_v1.h>
 
-/*Use Ardumega 2560 board*/
 
-//Pressure sensor configuration
-#define ADDR 40        // CS (CSB) pin when using SPI (Arduino numbering)              
-MS5611 MS5611(ADDR);
+/*Configuration du barometre pour l'altitude*/
+#define ADDR 40        /*Adresse*/             
+MS5611 MS5611(ADDR); /*Instanciation du capteur*/
 
-//Magnetic sensor configuration
+/*Configuration du magnetometre pour le cap*/
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 sensors_event_t event; 
 
-//Sensor Filters
+/*Filtre a moyenne glissante pour les mesures de pression et magnetique*/
 MeanFilter<uint32_t> pressureFilter(20);
 MeanFilter<uint32_t> magFilter(20);
 
-/*HMI pins*/
-int sensorPin = A0;    // select the input pin for the potentiometer
+/*Valeur du bouton poussoir pour enregistrer le cap*/
 int bpValue ;
+/*Pin du bouton poussoir*/
 int bpPin = A4;
+
+/*Valeur de l'interrupteur auto/manuel*/
 int  autoValue ;
+/*Pin de l'interrupteur*/
 int autoPin=A6;
+
+/*Valeur du potentiometre cap*/
 float  capValue ;
+/*Pin du potentiometre cap*/
 int capPin= PF0;
+
+/*Valeur du potentiometre altitude*/
 float  altiValue ;
+/*Pin du potentiometre altitude*/
 int altiPin = PF2;
 
-double mag_ref_x=0.0;
-double mag_ref_y=0.0;
+/*Valeur du cap en radiant*/
 double cap_rad=0.0;
+/*Valeur de reference du cap*/
 double cap_reference=0.0;
+/*Valeur de sortie du PID du cap*/
 double cap_output=0;
+/*Valeur du servo cap*/
 double cap_servo=20;
 
-int mode=1; /*0 manuel  1 regulé*/
-
+/*Instance de servo altimetre*/
 Servo servoAlti;
+/*Butees du servo d'altitude*/
+#define MIN_SERVO_ALTI 20
+#define MAX_SERVO_ALTI 160
+/*Instance de servo cap*/
 Servo servoCap;
+/*Butees du servo de cap*/
+#define MIN_SERVO_CAP 20
+#define MAX_SERVO_CAP 160
 
+
+
+/* PID du cap*/
 PID capPID(&cap_rad, &cap_output, &cap_reference,2.0,1.0,0.0, DIRECT);
 
 void setup() {
+
+  /*Initialisation du port serie*/
   Serial.begin(250000);
 
+  /*Initialisation des boutons*/
   pinMode(bpPin, INPUT);
   pinMode(autoPin, INPUT);
 
+  /*Configuration des pins des servos*/
   servoCap.attach(12); 
   servoAlti.attach(11);
 
-  /*Init PID*/
+  /*Initialisation du PID*/
   capPID.SetMode(AUTOMATIC);
+  /*Butees du PID*/
   capPID.SetOutputLimits(-10.0,10.0);
 
-  /* Initialise the pressure sensor */
+  /* Initialisation du capteur de pression */
   SPI.begin();
   MS5611.init();
-  //First pressure read to init pressureFilter
+  /*Premiere lecture de pression*/
   int result = MS5611.read(12);// 8 - 12 bit oversampling;
   if (result != 0)
   {
@@ -74,38 +107,31 @@ void setup() {
     pressureFilter.AddValue(MS5611.getPressure());
   }
 
-  /* Initialise the magnetic sensor */
+  /* Initialisation de capteur magnetique */
   if(!mag.begin())
   {
     /* There was a problem detecting the HMC5883 ... check your connections */
-    //Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
     while(1);
   }else
   {
-    //Serial.println("HMC5883 detected ...");
-    //First magnetic read
+    /*Premiere mesure magnetique*/
     mag.getEvent(&event);
     cap_rad = atan2(event.magnetic.y, event.magnetic.x);
   }
-  
-
-
-  
+ 
 }
 
+/*Fonction de lecture du cap*/
 void lectureCap(void)
 {
+  /*Valeur brute du cap*/
   double cap_raw=0.0;
-  /*Read magnetic sensor*/
+  /*Lecture de la valeur brute du magnetometre*/
   mag.getEvent(&event);
-  /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
-//  Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
-//  Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
-//  Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
- 
-  // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
-  // Calculate heading when the magnetometer is level, then correct for signs of axis.
+
+  /*Calcul du vecteur magnetique entre les axes X et Y*/
   cap_raw = atan2(event.magnetic.y, event.magnetic.x);
+  /*Gestion des dépassement autour des valeurs -pi et pi*/
   if( (cap_rad - cap_raw)>PI)
   {
     cap_rad = cap_raw + 2*PI;
@@ -117,21 +143,7 @@ void lectureCap(void)
     cap_rad = cap_raw;
   }
 
-  // Check for wrap due to addition of declination.
-  //if(cap_rad > 2*PI)
-    //cap_rad -= 2*PI;
-  
-  //cap_rad = (mag_ref_x - event.magnetic.x);//+(mag_ref_y - event.magnetic.y);
-   
-  // Correct for when signs are reversed.
-  //if(cap_rad < 0)
-    //cap_rad += 2*PI;
-    
-  // Check for wrap due to addition of declination.
-  //if(cap_rad > 2*PI)
-    //cap_rad -= 2*PI;
-   
-  // Convert radians to degrees for readability.
+  // Conversion du resultat en degree*/
   float headingDegrees = cap_rad * 180/M_PI; 
 
   //Affichage CAP
@@ -141,10 +153,11 @@ void lectureCap(void)
   //Serial.println();
 }
 
+/*Fonction de lecture de l'altitude*/
 void lectureAlti(void)
 {
-    /*Read pressure sensor*/
-  int result = MS5611.read(12);// 8 - 12 bit oversampling;
+  /*Lecture du capteur de pression*/
+  int result = MS5611.read(12);// mode 8 - 12 bit oversampling;
   if (result != 0)
   {
       Serial.print("Error in read: ");
@@ -162,9 +175,9 @@ void lectureAlti(void)
   }
 }
 
+/*Boucle principale du programme*/
 void loop()
 {
-
   /*Lecture des boutons et potentiomètres*/
   bpValue = digitalRead(bpPin);
   autoValue = digitalRead(autoPin);
@@ -172,21 +185,28 @@ void loop()
   altiValue = analogRead(altiPin);
 
   /*Mode manuel*/
-  if(mode==0)
+  if(autoValue==0)
   {
-      capValue = map(capValue, 0, 1023, 20, 160);     // scale it to use it with the servo (value between 0 and 180)
+      capValue = map(capValue, 0, 1023, MIN_SERVO_CAP,MAX_SERVO_CAP);     // scale it to use it with the servo (value between 0 and 180)
       servoCap.write(capValue);                  // sets the servo position according to the scaled value
 
       //Serial.print("Cap value: ");
       //Serial.println(capValue);
-      altiValue = map(altiValue, 0, 1023, 20, 160);     // scale it to use it with the servo (value between 0 and 180)
+      altiValue = map(altiValue, 0, 1023, MIN_SERVO_ALTI, MAX_SERVO_ALTI);     // scale it to use it with the servo (value between 0 and 180)
       servoAlti.write(altiValue);                  // sets the servo position according to the scaled value
-  }else
+  }else /*Mode automatique*/
   {
     lectureCap();
     lectureAlti();  
+    /*Lors d'un appui sur le bouton, on remet la consigne du PID au cap actuel*/
+    if(bpValue==1)
+    {
+      cap_reference=cap_rad;
+    }
+    /*execution du PID*/
     capPID.Compute();
-    cap_servo = map_double(cap_output,-10.0,10.0,20.0,160.0);     // scale it to use it with the servo [20;160]
+    /*Mise a l'echelle de la sortie du PID pour contraindre l'excursion du servo*/
+    cap_servo = map_double(cap_output,-10.0,10.0,MIN_SERVO_CAP,MAX_SERVO_CAP);     
     servoCap.write(cap_servo);                  // sets the servo position according to the scaled value
     Serial.print("in:"); Serial.print(cap_rad);
     Serial.print(" out:"); Serial.print(cap_output);
